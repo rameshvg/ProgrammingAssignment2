fd72bd7100686be8b4ee28a521ae6a21ef659272
RameshVG <vg.ramesh.in@gmail.com>
2015-02-22 19:01:05 -0500

    Correction

--- cacheSolve.R
+++ cacheSolve.R
@@ -4,7 +4,7 @@ cacheSolve <- function(x=matrix(), ...) {
       message("getting cached data")
       return(m)
     }
-    matrix<-x$get
+    matrix<-x$get()
     m<-solve(matrix, ...)
     x$setmatrix(m)
     m
